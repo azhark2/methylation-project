2562db1bf5d5b3b2365baf464c8e9bf4f6e230e4
Khandekar <khandekara2@ncbimac2058.ncbi.nlm.nih.gov>
2018-03-27 16:52:27 -0400

    pan-cancer and single profiles now working

--- methylation_profile.py
+++ methylation_profile.py
@@ -18,68 +18,56 @@ import os
 #File 3 --> Simple somatic mutation data from same cancer type(many samples)
 
 
+from multiprocessing import Pool
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+import pickle
+import os
+
 pathToMutations = '/data/khandekara2/mutation_data/processed_data/'
-pathToWGBS = '/data/khandekara2/profile_database'
-def plot_profile(*args, *kwargs)
-    gene = args[0]
-    tissue =
+pathToSingle = '/data/khandekara2/profile_database/'
+pathToPan = '/data/khandekara2/profile_database/pan_tissue/'
+pathToCancer = '/data/khandekara2/profile_database/cancer'
+
+def plot_profile(gene, profile_type, *args, **kwargs):
     if profile_type == 'pan':
-        pass
-    else if profile_type == 'single':
-        for file in os.listdir():
-            if file.startswith(tissue + '_' + gene):
-                df = pd.read_csv(file, sep='\t', header=False)
-                #add genomic location column
-                locations = []
-                for chrom, start, stop in zip(list(df.iloc[:, 0]), list(df.iloc[:, 1], list(df.iloc[:, 2])):
-                    locations.append((str(chrom), int(start), int(stop)))
-                df[len(df.columns)] = locations
-                x = np.array([i for i in range(1, df.shape[0] + 1)])
-                labels = list(set([i[1] for i in df.iloc[:,14])) #coordinate of cytosine as labels
-                labels.sort()
-                tss = df.iloc[:,10].mode() #transcription start site of TP53
-                x_labels = [i - tss for i in labels]
+        for file in os.listdir(pathToPan):
+            if file.startswith('pan_tissue_' + gene + '_cds_CpGs.bed'):
+                plot_pan(file, gene)
+    elif profile_type == 'single':
+        tissue = kwargs['tissue']
+        for file in os.listdir(pathToSingle):
+            if file.startswith(tissue + '_' + gene + '_cds_CpGs.bed'):
 
-                #load appropiate mutation data
-                if tissue_type.startswith('thymus'):
-                    mutation_dict = pickle.load(open('/data/khandekara2/mutation_data/processed_data/MALY_mutation_dict.pickle', 'rb'))
-                elif tissue_type.startswith('CD34'):
-                    mutation_dict = pickle.load(open('/data/khandekara2/mutation_data/processed_data/CLLE_mutation_dict.pickle', 'rb'))
-                else:
-                    mutation_dict = pickle.load(open(pathToMutations + tissue_type + '_mutation_dict.pickle', 'rb'))
 
-                mutations, mut_ratios, frequencies, num_samples = count_mutations(mutation_dict, df)
-
-                #construct profiles
-                %matplotlib inline
-                fig = plt.figure(figsize=(20, 2))
-                ax = fig.add_subplot(111)
-                ax.set_xticks(x)
-                ax.set_xticklabels(x_labels, rotation='vertical', fontsize=12)
-                ax.set_ylim((0.0, 1.1))
-                # ax.errorbar(x, y, yerr=y_err, fmt='o')
-                ax.set_xlabel('Distance from TSS', fontsize=14)
-                ax.set_ylabel('Methylation Ratio', fontsize=14)
-                ax.scatter(x, y2, color='red')
-                # ax.scatter(np.array(list(mutations)), np.array(mut_ratios), color='green')
-                trans = ax.get_xaxis_transform()
-                for u, v, f in zip(mutations, mut_ratios, frequencies):
-                    ax.annotate(str(round((f / num_samples) * 100, 1)), xy=(labels.index(u) + 1, v), xycoords=trans, arrowprops=dict(facecolor='black', shrink=0.5,))
-                ttl = ax.title
-                ttl.set_position([.5, 1.2])
-                ax.set_title('Methylation Profile of ' + gene_name + ' in Coding Region ' + tissue_type, fontsize=16)
-                # plt.gcf().subplots_adjust(bottom=0.15)
-                # fig.savefig('/Users/khandekara2/Documents/methylationProject/03_results/thymus_methylation_profile.png', bbox_inches='tight')
 
     else: #cancer + normal
-        pass
+        for file in os.listdir(pathToPan):
+            if file.startswith(kwargs['cancer_type'] + gene + '_cds_CpGs.bed'):
+                pass
+
+
+def add_location(df):
+    locations = []
+    for chrom, start, stop in zip(list(df.iloc[:, 0]), list(df.iloc[:, 1]), list(df.iloc[:, 2])):
+        locations.append((str(chrom), int(start), int(stop)))
+    df[len(df.columns)] = locations
+    return df
 
 def count_mutations(mutation_dict, df):
     #add mutated sites to profile
     mutations = [] #coordinate of mutated site
     mut_ratios = [] #methylation ratio of mutated site if available
     frequencies = [] #frequency of mutation(# of samples it occurred in)
-    for loc, sample, ratio in zip(df.iloc[:,14], df.iloc[:,3], df.iloc[:,4]):
+    #get total number of samples
+    samples = set([])
+    for l in mutation_dict.values():
+        for sample in l:
+            samples.add(sample)
+    num_samples = len(samples)
+    for loc, sample, ratio in zip(df.iloc[:, len(df.columns) - 1], df.iloc[:,3], df.iloc[:,4]):
         if loc in mutation_dict and loc[1] not in mutations:
             if sample in mutation_dict[loc]:
                 mut_ratios.append(ratio)
@@ -95,14 +83,77 @@ def count_mutations(mutation_dict, df):
                 mut_ratios.append(1.1)
             mutations.append(loc[1])
             frequencies.append(len(mutation_dict[loc2]))
-    #get total number of samples
-    samples = set([])
-    for l in mutation_dict.values():
-        for sample in l:
-            samples.add(sample)
 
-    num_samples = len(samples)
+    frequencies = [str(round((freq / num_samples) * 100, 1)) for freq in frequencies]
+    for i, freq in enumerate(frequencies):
+        if str(freq).split('.')[1] == '0':
+            frequencies[i] = str(freq).split('.')[0]
+
     return mutations, mut_ratios, frequencies, num_samples
+
+def plot_pan(file, gene):
+    df = pd.read_csv(pathToPan + file, sep='\t', header=None)
+    df.iloc[:, 2] = df.iloc[:, 2].apply(lambda x: x - 1)
+    df = add_location(df)
+    y = df.iloc[:, 3]
+    y_err = df.iloc[:, 4]
+    x = np.array([i for i in range(1, df.shape[0] + 1)])
+    labels = list(set([i[1] for i in df.iloc[:, len(df.columns) - 1]]))
+    labels.sort()
+    tss = df.iloc[2,9]
+    x_labels = [i - 7572926 for i in labels]
+    mutation_dict = pickle.load(open('/data/khandekara2/mutation_data/processed_data/all_mutation_dict.pickle', 'rb'))
+    mutations, mut_ratios, frequencies, num_samples = count_mutations(mutation_dict, df)
+    fig = plt.figure(figsize=(15, 2), dpi=300)
+    ax = fig.add_subplot(111)
+    ax.set_xticks(x)
+    ax.set_xticklabels(x_labels, rotation='vertical', fontsize=12)
+    ax.set_ylim((0.0, 1.1))
+    ax.errorbar(x, y, yerr=y_err, fmt='o', color='red')
+    ax.set_xlabel('Distance from Translation Start Site(bp)', fontsize=14)
+    ax.set_ylabel('Average Methylation Ratio', fontsize=14)
+    trans = ax.get_xaxis_transform()
+    for u, v, f in zip(mutations, mut_ratios, frequencies):
+        ax.annotate(f, xy=(labels.index(u) + 1, v), xycoords=trans, arrowprops=dict(facecolor='black', shrink=0.5,))
+    ttl = ax.title
+    ttl.set_position([.5, 1.2])
+    ax.set_title('Methylation Profile of %s Coding Region' % (gene), fontsize=16)
+
+
+def plot_single(df, file):
+    df = pd.read_csv(pathToSingle + file, sep='\t', header=None)
+    df.iloc[:, 2] = df.iloc[:, 2].apply(lambda x: x - 1)
+    #add genomic location column
+    df = add_location(df)
+    x = np.array([i for i in range(1, df.shape[0] + 1)])
+    labels = list(set([i[1] for i in df.iloc[:,14]])) #coordinate of cytosine as labels
+    labels.sort()
+    tss = df.iloc[2,10] #transcription start site of TP53
+    x_labels = [abs(i - tss) for i in labels]
+    y2 = np.array(df.iloc[:, 4]) #methylation ratio from normal tissue
+    #load appropiate mutation data
+    if tissue.startswith('Thymus'):
+        mutation_dict = pickle.load(open('/data/khandekara2/mutation_data/processed_data/MALY_mutation_dict.pickle', 'rb'))
+    elif tissue.startswith('CD34'):
+        mutation_dict = pickle.load(open('/data/khandekara2/mutation_data/processed_data/CLLE_mutation_dict.pickle', 'rb'))
+    else:
+        mutation_dict = pickle.load(open(pathToMutations + tissue.lower() + '_mutation_dict.pickle', 'rb'))
+    mutations, mut_ratios, frequencies, num_samples = count_mutations(mutation_dict, df)
+    #construct profiles
+    fig = plt.figure(figsize=(22, 2))
+    ax = fig.add_subplot(111)
+    ax.set_xticks(x)
+    ax.set_xticklabels(x_labels, rotation='vertical', fontsize=12)
+    ax.set_ylim((0.0, 1.1))
+    ax.set_xlabel('Distance from TSS', fontsize=14)
+    ax.set_ylabel('Methylation Ratio', fontsize=14)
+    ax.scatter(x, y2, color='red')
+    trans = ax.get_xaxis_transform()
+    for u, v, f in zip(mutations, mut_ratios, frequencies):
+        ax.annotate(f, xy=(labels.index(u) + 1, v), xycoords=trans, arrowprops=dict(facecolor='black', shrink=0.5,))
+    ttl = ax.title
+    ttl.set_position([.5, 1.2])
+    ax.set_title('Methylation Profile of ' + gene + ' in Coding Region of ' + tissue, fontsize=16)
 
 
 # #input WGBS file from cancer tissue
